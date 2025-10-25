4ee476bb0a9e6a6f328fa5ad4602fb90f3c52438
YASHNARBEK MELIKOV <yashnarbekmelikov@YASHNARBEKs-MacBook-Pro.local>
2025-10-25 22:56:02 +0500

    Fix tasks to empty

--- routes/addTask.js
+++ routes/addTask.js
@@ -1,20 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-let tasks = [
-   {
-      id: '1',
-      title: `Harry Potter ko'rish`,
-      body: `Bu kinoni ko'rishni anchadan beri xohlab yurgandim o'zi.`,
-      status: 'tugallangan',
-   },
-   {
-      id: '2',
-      title: `Shaxmat o'ynash`,
-      body: 'Kuzning noyabr oyi kelgandan shaxmatga borish',
-      status: 'kutilmoqda',
-   },
-];
+let tasks = [];
 
 router.get('/task/new', (req, res) => {
    res.render('handlebars/add-task', {
